4248a40b9ecb3724a12e7ce963cddff74a19823b
Kai Siren <coilysiren@gmail.com>
2024-10-07 14:59:37 -0700

    move scripts into folder

--- scipts/diffs.sh
+++ scipts/diffs.sh
@@ -4,7 +4,7 @@ function show_diff() {
   printf "\n"
   printf "%s" "$1"
   printf "\n"
-  jq . "$1" | sponge "$1" && jd -set "$1.template" "$1"
+  jq . "../$1" | sponge "../$1" && jd -set "../$1.template" "../$1"
 }
 
 cat diffs.txt | while read -r diff; do
